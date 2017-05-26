f6580d02fd094f0583439de198d58b3df0ecf8d6
Mark Gyozo Budai <xmarkbudaix@hotmail.com>
2017-05-26 10:38:26 +0200

    Implementing logging on TransportServiceImpl

--- src/main/java/com/markbudai/openfleet/services/implementations/TransportServiceImpl.java
+++ src/main/java/com/markbudai/openfleet/services/implementations/TransportServiceImpl.java
@@ -4,6 +4,8 @@ import com.markbudai.openfleet.dao.repositories.TransportRepository;
 import com.markbudai.openfleet.model.Employee;
 import com.markbudai.openfleet.model.Transport;
 import com.markbudai.openfleet.services.TransportService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,37 +18,48 @@ import java.util.stream.Collectors;
 @Service
 public class TransportServiceImpl implements TransportService {
 
+    private Logger logger = LoggerFactory.getLogger(TransportServiceImpl.class);
+
     private TransportRepository transportRepository;
 
     @Autowired
     public TransportServiceImpl(TransportRepository repository){
         this.transportRepository = repository;
+        logger.trace("{} service created.",TransportServiceImpl.class);
     }
 
     @Override
     public List<Transport> getAllTransports() {
-        return transportRepository.getAllTransports();
+        List<Transport> transports = transportRepository.getAllTransports();
+        logger.trace("Serving {} transports.",transports.size());
+        return transports;
     }
 
     @Override
     public void addTransport(Transport t) {
         transportRepository.addTransport(t);
+        logger.trace("Added transport {}",t);
     }
 
     @Override
     public List<Transport> getTransportByEmployee(Employee e) {
         List<Transport> transports = transportRepository.getAllTransports();
-        return transports.stream().filter(p->p.getEmployee().equals(e)).collect(Collectors.toList());
+        transports = transports.stream().filter(p->p.getEmployee().equals(e)).collect(Collectors.toList());
+        logger.trace("Serving {} transports for employee {}",transports.size(),e);
+        return transports;
     }
 
     @Override
     public Transport getTransportById(long id){
-        return transportRepository.getTransportById(id);
+        Transport transport = transportRepository.getTransportById(id);
+        logger.trace("Serving transport {} identified by {}",transport,id);
+        return transport;
     }
 
     @Override
     public void updateTransport(Transport t) {
         transportRepository.updateTransport(t);
+        logger.trace("Transport {} updated.",t);
     }
 
 }
