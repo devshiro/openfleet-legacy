26a05b07315222ed98ddac8c4b243acb952144ee
Mark Gyozo Budai <xmarkbudaix@hotmail.com>
2017-05-27 12:43:31 +0200

    DAO package-info javadoc fix

--- src/main/java/com/markbudai/openfleet/dao/package-info.java
+++ src/main/java/com/markbudai/openfleet/dao/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Created by Mark on 2017. 03. 24..
+ * Package containing interfaces and implementations for storing and managing {@link com.markbudai.openfleet.model} objects.
  */
 package com.markbudai.openfleet.dao;
